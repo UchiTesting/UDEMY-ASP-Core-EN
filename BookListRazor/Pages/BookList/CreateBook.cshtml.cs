07bcaa18f7e43c9cbd41f394d77ecc3768d12349
UchiTesing <uchitesting57@gmail.com>
2021-04-14 12:11:46 +0200

    Added server-side validation to the Createbook form.

--- BookListRazor/Pages/BookList/CreateBook.cshtml.cs
+++ BookListRazor/Pages/BookList/CreateBook.cshtml.cs
@@ -19,9 +19,25 @@ namespace BookListRazor.Pages.BookList
          _db = db;
       }
 
+      [BindProperty]
       public Book Book { get; set; }
+
       public void OnGet()
       {
+      }
+
+      public async Task<IActionResult> OnPost()
+      {
+         if (ModelState.IsValid)
+         {
+            await _db.Books.AddAsync(Book);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+         }
+         else
+         {
+            return Page();
+         }
       }
    }
 }
