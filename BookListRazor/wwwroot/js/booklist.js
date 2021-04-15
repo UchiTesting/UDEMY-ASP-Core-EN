cb8cf31526350326a95a7caf9d33908f44f98516
UchiTesing <uchitesting57@gmail.com>
2021-04-15 18:14:39 +0200

    Initial code for datatables. It does not have Delete button is not functional at this point.

---
+++ BookListRazor/wwwroot/js/booklist.js
@@ -0,0 +1,50 @@
+﻿var datatable;
+
+$(document).ready(function () {
+    loadDatatable();
+});
+
+function loadDatatable() {
+    datatable = $("#DT_load").DataTable({
+        "ajax": {
+            "url": "/apimvc/Book",
+            "type": "GET",
+            "daraSrc":"",
+            "dataType": "json"
+        }, 
+        "columns": [
+            {
+                "data": "title",
+                "width": "40%"
+            },
+            {
+                "data": "author",
+                "width": "20%"
+            },
+            {
+                "data": "isbn",
+                "width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a class="btn btn-sm btn-danger text-white"
+                                   style="cursor:pointer;">
+                                   Delete</a>
+
+                                <a href="/BookList/EditBook?id=${data}" 
+                                   class="btn btn-sm btn-success text-white"
+                                   style="cursor:pointer;">
+                                   Edit</a>
+                            </div>`
+                },
+                "width":"20%"
+            }
+        ],
+        "language": {
+            "emptyTable":"No data found"
+        },
+        "width": "100%"
+    });
+}
