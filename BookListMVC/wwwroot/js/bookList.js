f0d597862fff345000e720120f97c0f735832f13
UchiTesing <uchitesting57@gmail.com>
2021-04-16 15:25:34 +0200

    Created the bookList.js script. Adapted it to the project by changing the URL for AJAX calls.

---
+++ BookListMVC/wwwroot/js/bookList.js
@@ -0,0 +1,76 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDatatable();
+});
+
+function loadDatatable() {
+    dataTable = $("#DT_load").DataTable({
+        "ajax": {
+            "url": "/Books/GetAll/",
+            "type": "GET",
+            "dataType": "json"
+        },
+        "columns": [
+            {
+                "data": "title",
+                "width": "40%"
+            },
+            {
+                "data": "author",
+                "width": "20%"
+            },
+            {
+                "data": "isbn",
+                "width": "20%"
+            },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a class="btn btn-sm btn-danger text-white"
+                                   style="cursor:pointer;"
+                                   onclick="Delete('/apimvc/Book?id=${data}')">
+                                   Delete</a>
+
+                                <a href="/BookList/EditBook?id=${data}" 
+                                   class="btn btn-sm btn-success text-white"
+                                   style="cursor:pointer;">
+                                   Edit</a>
+                            </div>`
+                },
+                "width": "20%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found"
+        },
+        "width": "100%"
+    });
+}
+
+function Delete(apiUrl) {
+    swal({
+        title: "Are you sure?",
+        text: "This operation cannot be reverted.",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: apiUrl,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
