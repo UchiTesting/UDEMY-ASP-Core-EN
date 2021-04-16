58d713db15ae8751746262f500be3f4f7f31e3bf
UchiTesing <uchitesting57@gmail.com>
2021-04-16 11:20:14 +0200

    Changed actions in controller to use async methods.
    Implemented PUT for both apimvc and api endpoints for Book.

--- BookListRazor/Controllers/BookController.cs
+++ BookListRazor/Controllers/BookController.cs
@@ -1,8 +1,10 @@
 ﻿using BookListRazor.Models;
 
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-using System.Linq;
+using System.Threading.Tasks;
 
 namespace BookListRazor.Controllers
 {
@@ -15,27 +17,38 @@ namespace BookListRazor.Controllers
       public BookController(AppDbContext db) => _db = db;
 
       [HttpGet]
-      public IActionResult GetAll()
+      public async Task<IActionResult> GetAll()
       {
-         return Json(new { data = _db.Books.ToList() });
+         return Json(new { data = await _db.Books.ToListAsync() });
       }
 
       [HttpGet("{id}")]
-      public IActionResult Get(int id)
+      public async Task<IActionResult> Get(int id)
       {
-         return Json(new { data = _db.Books.SingleOrDefault(b => b.Id == id) });
+         return Json(new { data = await _db.Books.SingleOrDefaultAsync(b => b.Id == id) });
+      }
+
+      [HttpPost]
+      public async Task<IActionResult> Post(Book book)
+      {
+         if (!ModelState.IsValid) return BadRequest();
+
+         await _db.Books.AddAsync(book);
+         await _db.SaveChangesAsync();
+
+         return Created(new System.Uri($"{Request.GetDisplayUrl()}/{book.Id}"), book);
       }
 
       //[HttpDelete("{id}")]
       [HttpDelete]
-      public IActionResult Delete(int id)
+      public async Task<IActionResult> Delete(int id)
       {
-         var bookToDelete = _db.Books.Find(id);
+         var bookToDelete = await _db.Books.FindAsync(id);
 
          if (bookToDelete is null) return Json(new { success = false, message = "Error while deleting." });
 
          _db.Books.Remove(bookToDelete);
-         _db.SaveChanges();
+         await _db.SaveChangesAsync();
 
          return Json(new { success = true, message = "Deleted book successfully." });
       }
--- BookListRazor/Controllers/api/BookController.cs
+++ BookListRazor/Controllers/api/BookController.cs
@@ -1,8 +1,10 @@
 ﻿using BookListRazor.Models;
 
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-using System.Linq;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -21,24 +23,36 @@ namespace BookListRazor.Controllers.api
 
       // GET: api/<BookController>
       [HttpGet]
-      public IActionResult GetAll()
+      public async Task<IActionResult> GetAll()
       {
-         var list = _db.Books.ToList();
+         var list = await _db.Books.ToListAsync();
 
          if (list.Count < 1) return NotFound();
 
-         return Ok(list);
+         return Ok(new { data = list });
       }
 
       // GET api/<BookController>/5
       [HttpGet("{id}")]
-      public IActionResult Get(int id)
+      public async Task<IActionResult> Get(int id)
       {
-         var book = _db.Books.Find(id);
+         var book = await _db.Books.FindAsync(id);
 
          if (book is null) return NotFound();
 
          return Ok(book);
+      }
+
+      [HttpPost]
+      public async Task<IActionResult> Post(Book book)
+      {
+
+         if (!ModelState.IsValid) return BadRequest();
+
+         await _db.Books.AddAsync(book);
+         await _db.SaveChangesAsync();
+
+         return Created(new System.Uri($"{Request.GetDisplayUrl()}/{book.Id}"), book);
       }
    }
 }
